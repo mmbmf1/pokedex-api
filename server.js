1e108a8eff692ad7941827767a9f9f614c4a2fef
Michael <mmbmf.mm@gmail.com>
2019-12-05 11:06:46 -0600

    initial server code

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
