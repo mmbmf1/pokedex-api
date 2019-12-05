4fdd74b2103aad4627941fa19b671928c7ae5463
Michael <mmbmf.mm@gmail.com>
2019-12-05 13:04:01 -0600

    authorization in place

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
 
-console.log(process.env.API_TOKEN);
+console.log(process.env.API_TOKEN)
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-
-// app.use((req, res) => {
-//     res.send('Hello World!')
-// })
+app.use(morgan('dev'))
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 app.use(function validateBearerToken(req, res, next) {
+
+    // console.log(req.get('Authorization')) >> THIS DOESNT WORK!
+    // const bearerToken = req.query.Authorization.split(' ')[1];
+    //req.get('Authorization') did not work
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.query.Authorization;
+
     console.log('validate bearer token middleware')
+
+    // if(bearerToken !== apiToken) {
+    //     return res.status(401).json({ error: 'Unauthorized request' })
+    // }
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
     //move to next middlewear
     next()
 })
 
 function handleGetTypes(req, res) {
-    //CODE GOES HERE
     res.json(validTypes);
 }
 
