c4d73cf8cb26bfd723c52f328b7ca68275227118
Michael <mmbmf.mm@gmail.com>
2019-12-05 11:46:21 -0600

    token function added

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello World!')
+// app.use((req, res) => {
+//     res.send('Hello World!')
+// })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+    //move to next middlewear
+    next()
 })
+
+function handleGetTypes(req, res) {
+    //CODE GOES HERE
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000;
 
