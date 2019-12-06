cb70a14dc57af568aaf73c176de0bd22d11831fd
Michael <mmbmf.mm@gmail.com>
2019-12-06 05:06:45 -0600

    pokemon middleware

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
 
 console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 app.use(function validateBearerToken(req, res, next) {
 
@@ -30,17 +29,29 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-function handleGetTypes(req, res) {
-    res.json(validTypes);
-}
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
-app.get('/types', handleGetTypes)
+app.get('/types', function handleGetTypes(req, res) {
+    res.json(validTypes)
+})
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!');
-}
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
 
-app.get('/pokemon', handleGetPokemon)
+    if(req.query.name) {
+        response = response.filter(pokemon => 
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        )
+    }
+
+    if(req.query.type) {
+        response = response.filter(pokemon => 
+            pokemon.type.includes(req.query.type)
+        )
+    }
+
+    res.json(response)
+})
 
 const PORT = 8000;
 
