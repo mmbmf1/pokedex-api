ad063fdbd1218e5e9370be3d9e636b129956c2dc
Michael <mmbmf.mm@gmail.com>
2019-12-07 10:45:39 -0600

    updated for authorization header

--- server.js
+++ server.js
@@ -7,21 +7,19 @@ const POKEDEX = require('./pokedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-  // const authToken = req.query.Authorization
-  // console.log(authToken)
 
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  // move to the next middleware
   next()
 })
 
@@ -34,10 +32,8 @@ app.get('/types', function handleGetTypes(req, res) {
 app.get('/pokemon', function handleGetPokemon(req, res) {
   let response = POKEDEX.pokemon;
 
-  // filter our pokemon by name if name query param is present
   if (req.query.name) {
     response = response.filter(pokemon =>
-      // case insensitive searching
       pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
     )
   }
@@ -52,7 +48,17 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
